1700652b5498c0680eb4233316d092f930e20a96
Marcin Zagańczyk <39384751+mzaganczyk@users.noreply.github.com>
2021-03-01 17:20:42 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from App import *
 
 # DEFINE HOST NAME AND PORT OF YOUR SERVER
 HOST_NAME = socket.gethostname()
-PORT = 2137
+PORT = XXXX
 
 # SETTING UP THE SERVER
 server = App(isServer=True, host=HOST_NAME, port=PORT)
