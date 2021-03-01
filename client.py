3702cd085b81ac96cc661b35755bc9a3b090b187
Marcin Zagańczyk <39384751+mzaganczyk@users.noreply.github.com>
2021-03-01 17:21:03 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ from App import *
 from threading import Thread
 
 # DEFINE IP AND PORT OF YOUR SERVER
-IP = '192.168.8.100'
-PORT = 2137
+IP = ''
+PORT = XXXX
 
 # CONNECTING TO SERVER
 client = App()
